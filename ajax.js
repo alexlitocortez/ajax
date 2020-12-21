f67b6352cd76cc0c935a14cfe32742e5fb6641db
Alex Cortez <acim650@gmail.com>
2020-12-21 11:58:04 -0800

    ajax,js

--- ajax.js
+++ ajax.js
@@ -3,14 +3,14 @@ const key = document.getElementById('getText');
 
 key.addEventListener('keyup', nextText)
 
-const dogs = [];
+const dogs = [''];
 
 function nextText() {
     fetch('https://dog.ceo/api/breeds/list/all')
     // convert to JSON file (Javascript object notation)
     .then((res) => res.json())
     .then(data => console.log(data))
-    .then(data => dogs.push(...data))
+    .then(data => dogs.push(data))
 }
 
 function findMatches(wordToMatch, dogs) {
@@ -20,11 +20,11 @@ function findMatches(wordToMatch, dogs) {
     });
 }
 
-function 
+function displayMatches() {
+    const matchArray = findMatches(this.value, dogs);
+}
 
-
-
-
-
+key.addEventListener('change', displayMatches);
+key.addEventListener('keyup', displayMatches);
 
 
