23b36335672202f60c76d6dd7bca9ed43ba287e6
Alex Cortez <acim650@gmail.com>
2020-12-22 13:03:15 -0800

    ajax.js

--- ajax.js
+++ ajax.js
@@ -1,24 +1,22 @@
 
+const endpoint = 'https://dog.ceo/api/breeds/list/all';
 const key = document.getElementById('getText');
 const suggestions = document.querySelector('.suggestions');
 const dogs = [];
 
 
-key.addEventListener('keyup', nextText)
+fetch(endpoint)
+// convert to JSON file (Javascript object notation)
+.then(blob => blob.json())
+// Pushing data from API into an array
+.then(data => dogs.push(data));
+// Printing to console to see if "dogs" turned into an array
+console.log(dogs);
 
-
-function nextText() {
-    fetch('https://dog.ceo/api/breeds/list/all')
-    // convert to JSON file (Javascript object notation)
-    .then(res => res.json())
-    .then(function(res) {
-        res.dogs[i].forEach(element => {
-            dogs.push(element);
-        });
-    })
-    .then(() => console.log(dogs))
+function findMatches(wordToMatch, dogs) {
+    // filters the array into subsets
+    return dogs.filter(place => )
 }
-
 
 
 
