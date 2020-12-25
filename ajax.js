7189b1451708a976dcbfbccbcd8bce316bdac374
Alex Cortez <acim650@gmail.com>
2020-12-25 13:45:22 -0800

    ajax.js

---
+++ ajax.js
@@ -0,0 +1,18 @@
+const img = document.querySelector('img');
+const endpoint = 'https://api.unsplash.com/search/photos?query=dogs&client_id=sRf3a4tonFVZXP-ZR3Fz8dWoBECKIx5GT5cb3SbVo5A';
+const dogimg = document.getElementById('image_content');
+const dogs = document.getElementById('unsplash');
+
+dogs.addEventListener('click', fetchDogImg);
+
+function fetchDogImg() {
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            dogimg.innerHTML = `<img src="${data.file}"/>`;
+        })
+}
+
+
+
+
