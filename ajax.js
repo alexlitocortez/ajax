d61bdb71a88661c9261c157d57c74434f173e6b8
Alex Cortez <acim650@gmail.com>
2020-12-26 13:43:14 -0800

    ajax.js

--- ajax.js
+++ ajax.js
@@ -1,18 +1,22 @@
-const img = document.querySelector('img');
-<<<<<<< HEAD
 const endpoint = 'https://api.unsplash.com/search/photos?query=dogs&client_id=sRf3a4tonFVZXP-ZR3Fz8dWoBECKIx5GT5cb3SbVo5A';
 const dogimg = document.getElementById('image_content');
 const dogs = document.getElementById('unsplash');
 
-dogs.addEventListener('click', fetchDogImg);
 
-function fetchDogImg() {
+dogs.addEventListener('click', getImages);
+
+async function getImages(image) {
     fetch(endpoint)
-        .then(response => response.json())
-        .then(data => {
-            dogimg.innerHTML = `<img src="${data.file}"/>`;
+        .then(response => {
+            response.json();
         })
+        .then(data => {
+        console.log(data) //Prints results from response.json()` in getRequest
+        
+    })
 }
+
+// Find a way to insert API images and input into HTML
 
 
 
