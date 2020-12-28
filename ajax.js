94a7072299d7bb09ba58f2ff4f1a43c173ce553a
Alex Cortez <acim650@gmail.com>
2020-12-28 13:21:04 -0800

    ajax.js

--- ajax.js
+++ ajax.js
@@ -1,30 +1,19 @@
-const dogimg = document.getElementById('image_content');
-const dogs = document.getElementById('unsplash');
+// Use container
 
+const container = document.getElementById('container');
+const btn = document.getElementById('unsplash');
 
-async function getRandomDog() {
-    let url = "https://dog.ceo/api/breeds/image/random"
-    try {
-        let res = await fetch(url)
-        return await res.json();
-    }
-    catch(error) {
-        console.log(error);
-    }
+btn.addEventListener('click', fetchDogImage);
+
+function fetchDogImage() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => {
+            data.width = "450";
+            data.height = "450";
+            container.innerHTML = `<img src="${data.message}"/>`;
+        })
 }
-async function renderDogs() {
-    let dogs = await getRandomDog();
-    var dog = [];
-    dog = new Image(); // Create image object
-    dog.src = dogs.message;
-    dog.width = "600";
-    dog.height = "600";
-
-    document.dogimg.appendChild(dog);
-}
-// Find a way to insert API images and input into HTML
 
 
-
-
-
+// How to get object to perfectly fit  in container
