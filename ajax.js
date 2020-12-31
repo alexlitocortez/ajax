5d5a2aad1ed389d3e8c4e6a2f7bb638c9e3da9ea
Alex Cortez <acim650@gmail.com>
2020-12-31 14:27:33 -0800

    ajax.js

--- ajax.js
+++ ajax.js
@@ -13,19 +13,19 @@ function fetchDogImage() {
         })
         // Create a media condition that targets viewports at least 768px wide
         const mediaQuery = window.matchMedia('(max-width: 768px)');
+        const mediaQueryTwo = window.matchMedia('(max-width: 1400px)');
         
         if (mediaQuery.matches) {
             container.innerHTML = `<img src="${data.message}" width="408" height="197"/>`;
+        } else if (mediaQueryTwo.matches) {
+            container.innerHTML = `<img src="${data.message}" width="820" height="197"/>`;
         }
-}
+    };
 
+// width="820" height="197"
 
 
 
 // How to make Javascript adapt to media queries so the image fits the container perfectly for each screen size
 // Then do media queries
 // Then do the documentation for JS file
-
-
-
-
