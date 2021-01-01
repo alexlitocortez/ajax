b1d0e66247829f9bba6bf0721e86b13b3869fb66
Alex Cortez <acim650@gmail.com>
2020-12-31 19:08:38 -0800

    ajax.js

--- ajax.js
+++ ajax.js
@@ -2,6 +2,9 @@
 
 const container = document.getElementById('container');
 const btn = document.getElementById('unsplash');
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+const mediaQueryTwo = window.matchMedia('(max-width: 1400px)');
+const mediaQueryThree = window.matchMedia('(max-width: 375px)');
 
 btn.addEventListener('click', fetchDogImage);
 
@@ -9,22 +12,14 @@ function fetchDogImage() {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
         .then(data => {
-            const mediaQuery = window.matchMedia('(max-width: 768px)');
-            const mediaQueryTwo = window.matchMedia('(max-width: 1400px)');
             if (mediaQuery.matches) {
                 container.innerHTML = `<img src="${data.message}" width="408" height="197"/>`;
-            } else (mediaQueryTwo.matches) {
+            } else if (mediaQueryTwo.matches) {
                 container.innerHTML = `<img src="${data.message}" width="820" height="197"/>`;
-            };
-    });
+            }
+        });
+    };
 
 
-
-
-
-
-
-
-// How to make Javascript adapt to media queries so the image fits the container perfectly for each screen size
 // Then do media queries
 // Then do the documentation for JS file
