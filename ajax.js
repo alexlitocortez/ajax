59d67d081ed03143d16fb4752de938a2a0a0fad5
Alex Cortez <acim650@gmail.com>
2021-01-02 19:39:51 -0800

    ajax.js

--- ajax.js
+++ ajax.js
@@ -6,12 +6,16 @@ const mediaQueryOne = window.matchMedia('(max-width: 375px)');
 const mediaQueryTwo = window.matchMedia('(max-width: 768px)');
 const mediaQueryThree = window.matchMedia('(max-width: 1400px)')
 
+// When button is clicked the fetchDogImage() function runs
 btn.addEventListener('click', fetchDogImage);
 
+// Function for alling dog API and making sure images fit in the box they're in
 function fetchDogImage() {
+    // fetch API
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
         .then(data => {
+            // If and else statements for adjusting image based on dimensions
             if (mediaQueryOne.matches) {
                 container.innerHTML = `<img src="${data.message}" width="408" height="198"/>`;
             } else if (mediaQueryTwo.matches) {
@@ -22,8 +26,5 @@ function fetchDogImage() {
         });
     };
 
-
-// Then do media queries
-// Then do the documentation for JS file
 
 
